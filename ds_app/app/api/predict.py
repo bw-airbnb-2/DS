4f9b627e73429131fddf55374083e746fe3a5e1d
Matthew Means <matthewdmeans@Matthews-MacBook-Pro.local>
2020-09-24 13:26:13 -0700

    new model

--- ds_app/app/api/predict.py
+++ ds_app/app/api/predict.py
@@ -23,7 +23,7 @@ log = logging.getLogger(__name__)
 router = APIRouter()
 
 #load and read the file
-'''df = pd.read_csv("airbnb.csv", index_col=0)
+df = pd.read_csv("airbnb.csv", index_col=0)
 dataset = df.values
 X = dataset[:,0:12]
 y = dataset[:,12]
@@ -34,6 +34,7 @@ scaler_x.fit(X)
 xscale=scaler_x.transform(X)
 scaler_y.fit(y)
 yscale=scaler_y.transform(y)
+'''
 X = StandardScaler().fit_transform(X)
 y = StandardScaler().fit_transform(y.reshape(len(y),1))[:,0]
 X_train, X_test, y_train, y_test = train_test_split(xscale, yscale)
@@ -51,42 +52,44 @@ classifier = load_model('keras_model/keras_nn_model.h5')
 
 class AirBnB(BaseModel):
     """Data model to parse & validate airbnb measurements"""
-    {
-  "userId":1,
-  "name":"Chris",
-  "room_type":"large",
-  "location":"Japan",
-  "price":255.99,
-  "accommodates":3,
-  "bathrooms":2,
-  "bedrooms":2,
-  "beds":3,
-  "guests_included":2,
-  "minimum_nights":3,
-  "maximum_nights":6
-    }
+    zipcode: float = Field(..., example=10453)
+    property_type: str = Field(..., example='House')
+    square_footage: float = Field(..., example=4000)
+    bedrooms: float = Field(..., example=6)
+    bathrooms: float = Field(..., example=2)
+    review_score_rating: float = Field(..., example=70)
+    accommodates: float = Field(..., example=1)
+    cancellation_policy: str = Field(..., example='moderate')
+    cleaning_fee: float = Field(..., example=300)
+    free_parking: str = Field(..., example='yes')
+    wifi: str = Field(..., example='yes')
+    cable_tv: str = Field(..., example='yes')
 
     def to_df(self):
+        """Convert pydantic object to pandas dataframe with 1 row."""
         return pd.DataFrame([dict(self)])
 
 @router.post('/predict')
 async def predict_species(item: AirBnB):
-    """Random baseline predictions for classification problem"""
+    """Make random baseline predictions for classification problem."""
     X_new = item.to_df()
     log.info(X_new)
-    Xnew = np.array([
-        X_new['userId'].iloc[0], X_new['name'].iloc[0], X_new['room_type'].iloc[0], 
-        X_new['location'].iloc[0], X_new['price'].iloc[0], X_new['accommodates'].iloc[0],
-        X_new['bathrooms'].iloc[0], X_new['bedrooms'].iloc[0], X_new['beds'].iloc[0],
-        X_new['guests_included'].iloc[0], X_new['minimum_nights'].iloc[0], 
-        X_new['maximum_nights']]
-    )
-    Xnew = scaler_x.transform(Xnew)
-    y_pred = classifier.predict(Xnew)
+    model = tf.keras.models.load_model("keras_model2")
+    Dict = {'Apartment' : 1, 'House' : 0, 'flexible' : 0, 'moderate' : 1, 'strict' : 2, 'yes' : 1, 'no' : 0}
+    prop_type = Dict.get(X_new['property_type'].iloc[0])
+    can_pol = Dict.get(X_new['cancellation_policy'].iloc[0])
+    free_park = Dict.get(X_new['free_parking'].iloc[0])
+    wi_fi = Dict.get(X_new['wifi'].iloc[0])
+    cab_tv = Dict.get(X_new['cable_tv'].iloc[0])
+    Xnew = np.array([[X_new['zipcode'].iloc[0], X_new['square_footage'].iloc[0], X_new['bedrooms'].iloc[0], X_new['bathrooms'].iloc[0], X_new['review_score_rating'].iloc[0], X_new['accommodates'].iloc[0], X_new['cleaning_fee'].iloc[0], float(free_park), 
+                           float(wi_fi), float(cab_tv), float(prop_type), float(can_pol)]])
+    Xnew= scaler_x.transform(Xnew)
+    y_pred = model.predict(Xnew)
     y_pred = scaler_y.inverse_transform(y_pred)
     y_pred = float(y_pred[0][0])
+    #y_pred = float(random.randint(100, 500))
     return {
-        'prediction':y_pred
+        'prediction': y_pred
     }
 
     
