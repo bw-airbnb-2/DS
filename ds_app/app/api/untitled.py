9d642757ba69b83f2c0b2a4421754564529bea87
Matthew Means <matthewdmeans@Matthews-MacBook-Pro.local>
2020-09-24 15:03:55 -0700

    update

--- ds_app/app/api/untitled.py
+++ ds_app/app/api/untitled.py
@@ -42,15 +42,15 @@ class AirBnB(BaseModel):
     wifi: str = Field(..., example='yes')
     cable_tv: str = Field(..., example='yes')
     
-    def to_df(self):
-        """Convert pydantic object to pandas dataframe with 1 row."""
+def to_df(self):
+"""Convert pydantic object to pandas dataframe with 1 row."""
+    return pd.DataFrame([dict(self)])
 
 @router.post('/predict')
 async def predict(item: Item):
     """Make random baseline predictions for classification problem."""
     X_new = item.to_df()
     log.info(X_new)
-    model = tf.keras.models.load_model("keras_model2")
     Dict = {'Apartment' : 1, 'House' : 0, 'flexible' : 0, 'moderate' : 1, 'strict' : 2, 'yes' : 1, 'no' : 0}
     prop_type = Dict.get(X_new['property_type'].iloc[0])
     can_pol = Dict.get(X_new['cancellation_policy'].iloc[0])
@@ -58,9 +58,9 @@ async def predict(item: Item):
     wi_fi = Dict.get(X_new['wifi'].iloc[0])
     cab_tv = Dict.get(X_new['cable_tv'].iloc[0])
     Xnew = np.array([[X_new['zipcode'].iloc[0], X_new['square_footage'].iloc[0], X_new['bedrooms'].iloc[0],
-                      X_new['bathrooms'].iloc[0], 
-                           X_new['review_score_rating'].iloc[0], X_new['accommodates'].iloc[0], X_new['cleaning_fee'].iloc[0], float(free_park), 
-                           float(wi_fi), float(cab_tv), float(prop_type), float(can_pol)]])
+                      X_new['bathrooms'].iloc[0], X_new['review_score_rating'].iloc[0],
+                      X_new['accommodates'].iloc[0], X_new['cleaning_fee'].iloc[0], float(free_park), 
+                      float(wi_fi), float(cab_tv), float(prop_type), float(can_pol)]])
     Xnew= scaler_x.transform(Xnew)
     y_pred = model.predict(Xnew)
     y_pred = scaler_y.inverse_transform(y_pred)
