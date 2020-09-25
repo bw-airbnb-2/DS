ce37e9c55ebdf02a08168a113b01bbfd0cf03d04
Matthew Means <matthewdmeans@Matthews-MacBook-Pro.local>
2020-09-24 17:30:42 -0700

    Overhaul, prediction working

--- app/api/predict.py
+++ app/api/predict.py
@@ -1,59 +1,49 @@
-import random
-from fastapi import APIRouter
-import joblib
-import pandas as pd
-from pydantic import BaseModel, confloat
 import logging
 import random
+from fastapi import APIRouter
 import pandas as pd
 import numpy as np
-from fastapi import APIRouter
-import tensorflow as tf
+from pydantic import BaseModel, Field, validator
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from keras.models import load_model
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from keras.models import load_model
+from keras.models import save_model
 
 
 log = logging.getLogger(__name__)
 router = APIRouter()
 
-#load and read the file
-df = pd.read_csv("airbnb.csv", index_col=0)
+
+df = pd.read_csv('airbnb_bw2.csv', index_col=0)
 dataset = df.values
-X = dataset[:,0:10]
-y = dataset[:,10]
-y = np.reshape(y, (-1,1))
+X = dataset[:,0:12]
+y = dataset[:,12]
+y=np.reshape(y, (-1,1))
 scaler_x = MinMaxScaler()
 scaler_y = MinMaxScaler()
 scaler_x.fit(X)
 xscale=scaler_x.transform(X)
 scaler_y.fit(y)
 yscale=scaler_y.transform(y)
-'''
-X = StandardScaler().fit_transform(X)
+'''X = StandardScaler().fit_transform(X)
 y = StandardScaler().fit_transform(y.reshape(len(y),1))[:,0]
 X_train, X_test, y_train, y_test = train_test_split(xscale, yscale)
 model = Sequential()
-model.add(Dense(12, input_dim=10, kernel_initializer='normal', activation='relu'))
+model.add(Dense(12, input_dim=12, kernel_initializer='normal', activation='relu'))
 model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='linear'))
 model.compile(loss='mse', optimizer='adam', metrics=['mse','mae'])
-history = model.fit(X_train, y_train, epochs=50, batch_size=50,  verbose=1, validation_split=0.2)
-model.save('keras_nn_model')
-'''
-#classifier = joblib.load('app/api/classifier.joblib')
-#print('Pickled model loaded!')
-classifier = load_model('keras_model/keras_nn_model.h5')
+history = model.fit(X_train, y_train, epochs=150, batch_size=50,  verbose=1, validation_split=0.2)'''
 
-class AirBnB(BaseModel):
-    """Data model to parse & validate airbnb measurements"""
-    
+
+class Item(BaseModel):
+    """Use this data model to parse the request body JSON."""
+
     zipcode: float = Field(..., example=10453)
     property_type: str = Field(..., example='House')
     square_footage: float = Field(..., example=4000)
@@ -71,8 +61,11 @@ class AirBnB(BaseModel):
         """Convert pydantic object to pandas dataframe with 1 row."""
         return pd.DataFrame([dict(self)])
 
+
+
+
 @router.post('/predict')
-async def predict_species(item: AirBnB):
+async def predict(item: Item):
     """Make random baseline predictions for classification problem."""
     X_new = item.to_df()
     log.info(X_new)
@@ -83,8 +76,10 @@ async def predict_species(item: AirBnB):
     free_park = Dict.get(X_new['free_parking'].iloc[0])
     wi_fi = Dict.get(X_new['wifi'].iloc[0])
     cab_tv = Dict.get(X_new['cable_tv'].iloc[0])
-    Xnew = np.array([[X_new['zipcode'].iloc[0], X_new['square_footage'].iloc[0], X_new['bedrooms'].iloc[0], X_new['bathrooms'].iloc[0], X_new['review_score_rating'].iloc[0], X_new['accommodates'].iloc[0], X_new['cleaning_fee'].iloc[0], float(free_park), 
-                           float(wi_fi), float(cab_tv), float(prop_type), float(can_pol)]])
+    Xnew = np.array([[X_new['zipcode'].iloc[0], X_new['square_footage'].iloc[0], X_new['bedrooms'].iloc[0],
+                      X_new['bathrooms'].iloc[0], X_new['review_score_rating'].iloc[0],
+                      X_new['accommodates'].iloc[0], X_new['cleaning_fee'].iloc[0], float(free_park), 
+                      float(wi_fi), float(cab_tv), float(prop_type), float(can_pol)]])
     Xnew= scaler_x.transform(Xnew)
     y_pred = model.predict(Xnew)
     y_pred = scaler_y.inverse_transform(y_pred)
@@ -93,20 +88,3 @@ async def predict_species(item: AirBnB):
     return {
         'prediction': y_pred
     }
-
-    
-
-
-@router.get('/random')
-def random_airbnb():
-    """Return a random airbnb species"""
-    return random.choice([{"userId":1, "name":"Chris", "room_type":"large", 
-    "location":"Japan",
-    "price":255.99,
-    "accommodates":3,
-    "bathrooms":2,
-    "bedrooms":2,
-    "beds":3,
-    "guests_included":2,
-    "minimum_nights":3,
-    "maximum_nights":6}])
